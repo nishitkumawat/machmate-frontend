c9f8938f67b6423a5d70d679901493487c19972b
nishitkumawat <nishit1060@gmail.com>
2025-09-30 10:37:23 +0530

    Commit on 30/09 5

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/project/', // 👈 Important for Netlify (relative paths)
+  base: "./", // 👈 Important for Netlify (relative paths)
 });
